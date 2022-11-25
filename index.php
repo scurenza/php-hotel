27d5b777b83edc90f519cb34f1db41b594819118
scurenza <fedechiar@hotmail.it>
2022-11-25 16:25:53 +0100

    problemi con valori booleani

--- index.php
+++ index.php
@@ -58,23 +58,43 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista Hotels</title>
+
+    <!-- Bootstrap -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
     <section class="hotels">
         <h1>Lista Hotels</h1>
-        <ul>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $hotel) { ?>
+                    <tr>
+                        <?php foreach ($hotel as $key => $item) {
+                            if ($hotel[$parking]) {
+                                $hotel[$parking] = "presente";
+                            } else {
+                                $hotel[$parking] = 'assente';
+                            } ?>
 
-            <?php foreach ($hotels as $hotel) {
-                foreach ($hotel as $item) { ?>
-                    <li>
-                        <?php echo $item; ?>
-                    </li>
-            <?php
-                }
-            } ?>
-
-        </ul>
+                            <th scope="row"> <?php echo $item ?> </th>
+                        <?php } ?>
+                    </tr>
+                <?php }
+                ?>
+            </tbody>
+        </table>
     </section>
 </body>
 
